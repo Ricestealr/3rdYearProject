d44c827363e4f69ab89ffbd14f98bb6ebf5b4b4c
Sergi Rene <sergi@datahop.network>
2022-01-25 08:34:02 +0100

    config tag

--- docker_netconf/testOven.py
+++ docker_netconf/testOven.py
@@ -9,10 +9,12 @@ with manager.connect(host='localhost', port=830, username='netconf',password='ne
     food_in_rpc = '''<insert-food xmlns='urn:sysrepo:oven'><time>on-oven-ready</time></insert-food>'''
     result = m.dispatch(to_ele(food_in_rpc))
     oven_config = '''
-        <oven xmlns='urn:sysrepo:oven'>
-            <turned-on>true</turned-on>
-            <temperature>200</temperature>
-        </oven>
-    '''
+        <config xmlns='urn:ietf:params:xml:ns:netconf:base:1.0'>
+            <oven xmlns='urn:sysrepo:oven'>
+                <turned-on>true</turned-on>
+                <temperature>200</temperature>
+            </oven>
+        </config>
+    '''    
     m.edit_config(config = oven_config, target='running', default_operation='merge')
 
