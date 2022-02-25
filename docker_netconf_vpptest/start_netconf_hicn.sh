c3aefc4285b9c68db6c214d4e0a30d649a7b7f24
Sergi Rene <sergi@datahop.network>
2022-02-25 17:29:59 +0100

    docker update

--- docker_netconf_vpptest/start_netconf_hicn.sh
+++ docker_netconf_vpptest/start_netconf_hicn.sh
@@ -4,6 +4,6 @@
 #netopeer2-server -d -v 4
 #sysrepoctl -i /netconf/sysrepo/examples/plugin/oven.yang
 sysrepoctl -i /netconf/hicn/ctrl/sysrepo-plugins/yang/ondemand.yang
-vpp -c startup_nohicn.conf
+vpp -c startup_hicn.conf
 netopeer2-server
 sysrepo-plugind -d -v 4
